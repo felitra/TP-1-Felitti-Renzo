45c2c012eecba3194bf4fc0f2b25383ee7a73590
felitra <renzo.felitti@yahoo.com>
2015-04-13 23:39:02 -0300

    Modificacion al codigo y nueva salida

--- Convertidor a Farenheit.c
+++ Convertidor a Farenheit.c
@@ -2,7 +2,7 @@
  ============================================================================
  Name        : Convertidor.c
  Author      : Felitti Renzo
- Version     : 1.0
+ Version     : 1.1
  Copyright   : Your copyright notice
  Description : Convertidor a Farenheit in C, Ansi-style
  ============================================================================
@@ -10,26 +10,17 @@
 
 #include <stdio.h>
 
- void imprimirTabla(int array[] , int size){
-
-	 printf("Celcius a Farenheit\n\n");
-	 int farenheit=0;
-	 int i=0;
-
-	 while(i<size){
-		 farenheit=((array[i] * 9)/5) + 32;
-		 printf("%d  ---> %d\n",array[i],farenheit);
-		 i++;
-	 }
-}
 
 int main() {
 
-	int gradosCelcius[] ={0,20,40,60,80,100,120,140,160,180,200,220,240,260,280,300};
 
-	int size = sizeof(gradosCelcius)/sizeof(int);
+	 printf(" Celcius a Farenheit\n\n");
+	 int farenheit;
 
-	imprimirTabla(gradosCelcius,size);
+	 for(farenheit=0; farenheit<=300 ; farenheit ++){
+
+		 printf("%3d°C  ----> %.2f°F \n",farenheit, (9.0/5.0) * farenheit  + 32);
+	 }
 
 
 	return 0;
